0f2eb40c1ad7cae85ef4d4f722206ab184198dd3
IntesIt <intes@it.com>
2022-12-14 14:35:58 +0700

    update margin style, add heart icon

--- src/components/layout/header/right-section/index.tsx
+++ src/components/layout/header/right-section/index.tsx
@@ -2,14 +2,21 @@ import * as React from "react";
 import Account from "./account";
 import CartPopover from "./cart-popover";
 import SearchPopover from "../seach-popover";
-
+import Favourite from "./favourite";
 const RightSection = () => {
   return (
     <div className="grid gap-4 content-center">
-      <div className="flex ml-auto mr-6 space-x-4">
-        <SearchPopover className="hidden md:block"/>
-        <Account/>
-        <CartPopover/>
+      <div className="flex  mr-6 ml-auto">
+        <SearchPopover className="hidden md:block mx-4" />
+        <div className="mx-4">
+          <Account />
+        </div>
+        <div className="mx-4">
+          <Favourite />
+        </div>
+        <div className="mx-4">
+          <CartPopover />
+        </div>
       </div>
     </div>
   );
--- src/components/layout/header/seach-popover/search-modal.tsx
+++ src/components/layout/header/seach-popover/search-modal.tsx
@@ -1,4 +1,4 @@
-import React, { ReactEventHandler } from "react";
+import React from "react";
 type SearchProps={
   isVisible:boolean,
   onClose:()=>void
@@ -6,13 +6,14 @@ type SearchProps={
 const SearchModal:React.FC<SearchProps> = ({isVisible, onClose}) => {
   if (!isVisible) return null;
   const handleClose = (e:any) => {
+    
     if(e.target.id === 'wrapper') onClose();
   }
   return (
     <div id='wrapper' onClick={handleClose} className="animate-crescendo fixed z-50 inset-0 bg-black bg-opacity-75 flex justify-center items-center m-0">
       <button onClick={() => onClose()} className="fixed top-[50%] right-[50px] text-white text-xl place-self-end  mobile:right-[15px]">X</button>
       <input
-            type="text"
+            type="search"
             className="form input px-4 py-4 w-[60vw] bg-black bg-opacity-0 focus:outline-none text-white text-[50px] text-center border-b-2 mobile:text-[30px]"
             placeholder="Recherche..."
             />
