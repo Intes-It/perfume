996febfb4c24ea81ea36204bbc6bf7bc2bdd6220
luckystar360 <ngocnhat95@gmail.com>
2022-12-26 00:05:30 +0700

    update home page for test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useMemo, useState } from "react";
+import { useWindowSize } from "react-use";
 import { Container } from "@components";
 
-const slideInfo = [
-  {
-    url: "https://mldn3w3pos1n.i.optimole.com/cb:453S~5a2d9/w:auto/h:auto/q:mauto/id:aea1de3b3657bf3ad37e5d772129bfc3/https://naturefeerique.fr/Slide1.0-scaled.jpg",
-    caption:
-      "Pour Noël, optez pour un cadeau 100% artisanal et local. \nDécouvrir notre sélection Noël",
-    text: "J'ADORE",
-    href: "",
-  },
-  {
-    url: "https://mldn3w3pos1n.i.optimole.com/cb:453S~5a2d9/w:auto/h:auto/q:mauto/id:aea1de3b3657bf3ad37e5d772129bfc3/https://naturefeerique.fr/Slide1.0-scaled.jpg",
-    text: "",
-    caption: "",
-    href: "",
-  },
-  {
-    url: "https://mldn3w3pos1n.i.optimole.com/cb:453S~5a2d9/w:auto/h:auto/q:mauto/id:7c31e1c30840e51557124a176e48f406/https://naturefeerique.fr/Box-a-composer1-scaled.jpg",
-    text: "OUI",
-    caption:
-      "Un cadeau personnalisable avec nos \nbox à composer avec 2 ou 3 produits au choix.",
-    href: "",
-  },
-];
+import { homeSlideInfo, productItem } from "src/utils/fakeData";
+import ProductItem from "@components/product-item";
 
 const Home: React.FC = () => {
+  const { width, height } = useWindowSize();
+  const [state, setState] = useState({
+    homeSlideData: homeSlideInfo,
+    bestSellingProducts: productItem,
+  });
+
+  const { homeSlideData, bestSellingProducts } = state;
+
+  const stepProductSlide = useMemo(() => (width > 740 ? 4 : 1), [width]);
+
   useEffect(() => {
     // setTheme('light');
   }, []);
@@ -37,7 +28,7 @@ const Home: React.FC = () => {
         data-bs-ride="carousel"
       >
         <div className="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
-          {slideInfo?.map((item: any, index: number) => (
+          {homeSlideData?.map((item: any, index: number) => (
             <button
               key={index}
               type="button"
@@ -50,7 +41,7 @@ const Home: React.FC = () => {
           ))}
         </div>
         <div className="carousel-inner relative w-full overflow-hidden ">
-          {slideInfo?.map((item: any, index: number) => (
+          {homeSlideData?.map((item: any, index: number) => (
             <div
               key={index}
               className={`carousel-item ${
@@ -67,7 +58,7 @@ const Home: React.FC = () => {
                 )}
                 {item?.text && (
                   <button
-                    className="rounded-full h-12 bg-white text-lg min-w-[140px]"
+                    className="rounded-full h-12 bg-white text-lg min-w-[140px] border-white border hover:bg-transparent hover:text-white"
                     ref={item?.ref}
                   >
                     {item?.text}
@@ -106,6 +97,95 @@ const Home: React.FC = () => {
           ></span>
           <span className="visually-hidden">Next</span>
         </button>
+      </div>
+
+      <div className="my-16 md:min-h-[15vw] min-h-[12rem] w-full grid content-center bg-[#cec2b9]">
+        <span className="text-center md:text-[36px] text-[24px] m-2 text-white font-bold">
+          CHOISISSEZ DES PRODUITS NATURELS QUI FAVORISENT VOTRE BIEN-ÊTRE ET
+          VOUS FONT VOUS SENTIR BIEN
+        </span>
+      </div>
+
+      {/* best sales */}
+      <div>
+        <div className="w-full grid">
+          <span className="text-center text-[32px] font-semibold tracking-wide text-[#383e42]">
+            NOS MEILLEURES VENTES
+          </span>
+        </div>
+
+        <div
+          id="bestSallingProductSlide"
+          className="carousel carousel-dark slide relative overflow-hidden my-3"
+          data-bs-ride="carousel"
+        >
+          <div className="carousel-inner relative w-full overflow-hidden mx-auto">
+            {bestSellingProducts?.map((product, index) => {
+              return (
+                index % stepProductSlide == 0 && (
+                  <div
+                    key={index}
+                    className={`carousel-item ${
+                      index === 0 ? "active" : ""
+                    } relative float-left w-full`}
+                  >
+                    <div
+                      className={`grid md:grid-cols-4 grid-cols-1 text-center space-x-4 mx-8`}
+                    >
+                      {bestSellingProducts
+                        ?.slice(index, index + stepProductSlide)
+                        ?.map((item, index2) => {
+                          return (
+                            <div key={index2} className="border border-gray">
+                              <ProductItem
+                                favorites={() => console.log(index2)}
+                                title={item.title}
+                                price={item.price}
+                                image={item.image}
+                              />
+                            </div>
+                          );
+                        })}
+                    </div>
+                  </div>
+                )
+              );
+            })}
+            {/* <div className="carousel-item active relative float-left w-full">
+              <div className="bg-red-300 h-20"></div>
+            </div>
+            <div className="carousel-item relative float-left w-full">
+              <div className="bg-blue-300 h-20"></div>
+            </div>
+            <div className="carousel-item relative float-left w-full">
+              <div className="bg-green-300 h-20"></div>
+            </div> */}
+          </div>
+          <button
+            className="carousel-control-prev absolute top-0 bottom-0 left-[calc(-7.5vw_+_1rem)] flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline"
+            type="button"
+            data-bs-target="#bestSallingProductSlide"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon inline-block bg-no-repeat"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next absolute top-0 bottom-0 right-[calc(-7.5vw_+_1rem)] flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline"
+            type="button"
+            data-bs-target="#bestSallingProductSlide"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon inline-block bg-no-repeat"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
       </div>
     </Container>
   );
--- src/utils/fakeData.ts
+++ src/utils/fakeData.ts
@@ -1,62 +1,108 @@
-
-export const productItem=[
-    {
-        image: "https://naturefeerique.fr/wp-content/uploads/2022/03/demaquillant-cocoon-1.jpg",
-        title:"Démaquillant Cocoon",
-        price:"15,15",
-        vote:"100",
-        rating:"3"
-    },
-    {
-        image: "https://naturefeerique.fr/wp-content/uploads/2022/03/deodorant-plume.jpg",
-        title:"Déodorant Plume",
-        price:"14,5",
-        vote:"150",
-        rating:"3,6"
-    },
-    {
-        image: "https://naturefeerique.fr/wp-content/uploads/2022/03/perles-de-soin-2.jpg",
-        title:"Perles de soin",
-        price:"10,90",
-        vote:"300",
-        rating:"4"
-    },
-    {
-        image: "https://naturefeerique.fr/wp-content/uploads/2022/03/perles-demaquillantes-1.jpg",
-        title:"Perles démaquillantes",
-        price:"9,50",
-        vote:"180",
-        rating:"5"
-    },
-    {
-        image: "https://naturefeerique.fr/wp-content/uploads/2022/03/soin-precieux.jpg",
-        title:"Soin Précieux",
-        price:"17,90",
-        vote:"200",
-        rating:"4,8"
-    },
-   
+export const productItem = [
+  {
+    image:
+      "https://naturefeerique.fr/wp-content/uploads/2022/03/demaquillant-cocoon-1.jpg",
+    title: "Démaquillant Cocoon",
+    price: "15,15",
+    vote: "100",
+    rating: "3",
+  },
+  {
+    image:
+      "https://naturefeerique.fr/wp-content/uploads/2022/03/deodorant-plume.jpg",
+    title: "Déodorant Plume",
+    price: "14,5",
+    vote: "150",
+    rating: "3,6",
+  },
+  {
+    image:
+      "https://naturefeerique.fr/wp-content/uploads/2022/03/perles-de-soin-2.jpg",
+    title: "Perles de soin",
+    price: "10,90",
+    vote: "300",
+    rating: "4",
+  },
+  {
+    image:
+      "https://naturefeerique.fr/wp-content/uploads/2022/03/perles-demaquillantes-1.jpg",
+    title: "Perles démaquillantes",
+    price: "9,50",
+    vote: "180",
+    rating: "5",
+  },
+  {
+    image:
+      "https://naturefeerique.fr/wp-content/uploads/2022/03/soin-precieux.jpg",
+    title: "Soin Précieux",
+    price: "17,90",
+    vote: "200",
+    rating: "4,8",
+  },
+  {
+    image:
+      "https://naturefeerique.fr/wp-content/uploads/2022/03/soin-precieux.jpg",
+    title: "Soin Précieux",
+    price: "17,90",
+    vote: "200",
+    rating: "4,8",
+  },
+  {
+    image:
+      "https://naturefeerique.fr/wp-content/uploads/2022/03/soin-precieux.jpg",
+    title: "Soin Précieux",
+    price: "17,90",
+    vote: "200",
+    rating: "4,8",
+  },
+  {
+    image:
+      "https://naturefeerique.fr/wp-content/uploads/2022/03/soin-precieux.jpg",
+    title: "Soin Précieux",
+    price: "17,90",
+    vote: "200",
+    rating: "4,8",
+  },
 ];
-export const productPrice = [
-    "0€ — 10€",
-    "11€ — 20€",
-    "21€ — 50€",
-];
+export const productPrice = ["0€ — 10€", "11€ — 20€", "21€ — 50€"];
 export const productFilter = [
-    "Trier Par...",
-    "Alphabétique (A à Z)",
-    "Alphabétique (Z à A)",
-    "Prix Croissant",
-    "Prix Décroissant",
-    "Notes Moyennes",
-    "Nombres D'avis",
+  "Trier Par...",
+  "Alphabétique (A à Z)",
+  "Alphabétique (Z à A)",
+  "Prix Croissant",
+  "Prix Décroissant",
+  "Notes Moyennes",
+  "Nombres D'avis",
 ];
 
-export const colourOptions=[
-    { value: 'orange', label: 'Orange'},
-    { value: 'yellow', label: 'Yellow'},
-    { value: 'green', label: 'Green'},
-    { value: 'forest', label: 'Forest'},
-    { value: 'slate', label: 'Slate'},
-    { value: 'silver', label: 'Silver'},
-  ];
+export const colourOptions = [
+  { value: "orange", label: "Orange" },
+  { value: "yellow", label: "Yellow" },
+  { value: "green", label: "Green" },
+  { value: "forest", label: "Forest" },
+  { value: "slate", label: "Slate" },
+  { value: "silver", label: "Silver" },
+];
+
+export const homeSlideInfo = [
+  {
+    url: "https://mldn3w3pos1n.i.optimole.com/cb:453S~5a2d9/w:auto/h:auto/q:mauto/id:aea1de3b3657bf3ad37e5d772129bfc3/https://naturefeerique.fr/Slide1.0-scaled.jpg",
+    caption:
+      "Pour Noël, optez pour un cadeau 100% artisanal et local. \nDécouvrir notre sélection Noël",
+    text: "J'ADORE",
+    href: "",
+  },
+  {
+    url: "https://mldn3w3pos1n.i.optimole.com/cb:453S~5a2d9/w:auto/h:auto/q:mauto/id:aea1de3b3657bf3ad37e5d772129bfc3/https://naturefeerique.fr/Slide1.0-scaled.jpg",
+    text: "",
+    caption: "",
+    href: "",
+  },
+  {
+    url: "https://mldn3w3pos1n.i.optimole.com/cb:453S~5a2d9/w:auto/h:auto/q:mauto/id:7c31e1c30840e51557124a176e48f406/https://naturefeerique.fr/Box-a-composer1-scaled.jpg",
+    text: "OUI",
+    caption:
+      "Un cadeau personnalisable avec nos \nbox à composer avec 2 ou 3 produits au choix.",
+    href: "",
+  },
+];
