003a5dee10b6f8523df95b26b8913142bbfb369d
Cyril OSMANI <secur365.suite@gmail.com>
2023-02-22 12:31:34 +0000

    fix slide

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,6 +16,7 @@ import { Layout } from "@components";
 import GlobalStyle from "@styles/globalStyles";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import 'tw-elements';
 
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
