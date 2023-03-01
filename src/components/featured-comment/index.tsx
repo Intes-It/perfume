39f2fa9f1a3e950e5627f7ad180dce5c7976ee75
Nhat Dong <ngocnhat95@gmail.com>
2023-03-01 15:32:31 +0700

    update carousel

--- src/components/featured-comment/index.tsx
+++ src/components/featured-comment/index.tsx
@@ -12,67 +12,120 @@ export const FeaturedComments: React.FC<FeaturedCommentsProps> = ({
   return (
     <div
       id="featuredCommentsSlide"
-      className="carousel carousel-dark slide relative overflow-hidden my-3"
-      data-bs-ride="carousel"
+      className="relative"
+      data-te-carousel-init
+      data-te-carousel-slide
     >
-      <div className="carousel-inner relative w-full overflow-hidden mx-auto">
+      <div
+        className="absolute right-0 bottom-0 left-0 z-[2] mx-[15%] mb-4 flex list-none justify-center p-0"
+        data-te-carousel-indicators
+      >
+        <button
+          type="button"
+          data-te-target="#featuredCommentsSlide"
+          data-te-slide-to="0"
+          data-te-carousel-active
+          className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
+          aria-current="true"
+          aria-label="Slide 1"
+        ></button>
+        <button
+          type="button"
+          data-te-target="#featuredCommentsSlide"
+          data-te-slide-to="1"
+          className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
+          aria-label="Slide 2"
+        ></button>
+        <button
+          type="button"
+          data-te-target="#featuredCommentsSlide"
+          data-te-slide-to="2"
+          className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
+          aria-label="Slide 3"
+        ></button>
+      </div>
+      <div className="relative w-full overflow-hidden after:clear-both after:block after:content-['']">
         {comments?.map((comment: MemberComment, index: number) => {
+          const active = index === 0 ? true : undefined;
+          const hidden = index !== 0;
           return (
             <div
               key={index}
-              className={`carousel-item ${
-                index === 0 ? "active" : ""
-              } relative float-left w-full`}
+              className={`relative ${
+                hidden && "hidden"
+              } float-left -mr-[100%] w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none`}
+              data-te-carousel-item
+              data-te-carousel-active={active}
             >
               <div className="text-center mobile:mx-[10vw] mx-[6vw]">
                 <div>
-                    <span className="text-[#603113] text-[20px] mobile:text-[14px] italic">
+                  <span className="text-[#603113] text-[20px] mobile:text-[14px] italic">
                     {comment?.text}
-                    </span>
+                  </span>
                 </div>
                 <div className="mt-6">
-                    <span className="text-[#603113] text-[20px] mobile:text-[14px]">
+                  <span className="text-[#603113] text-[20px] mobile:text-[14px]">
                     {comment?.member}
-                    </span>
-                </div> 
-              </div> 
+                  </span>
+                </div>
+              </div>
             </div>
           );
         })}
-        {/* <div className="carousel-item active relative float-left w-full">
-              <div className="bg-red-300 h-20"></div>
-            </div>
-            <div className="carousel-item relative float-left w-full">
-              <div className="bg-blue-300 h-20"></div>
-            </div>
-            <div className="carousel-item relative float-left w-full">
-              <div className="bg-green-300 h-20"></div>
-            </div> */}
       </div>
       <button
-        className="carousel-control-prev absolute top-0 bottom-0 left-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline"
+        className="absolute top-0 bottom-0 left-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-gray-500 opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-gray-600 hover:no-underline hover:opacity-90 hover:outline-none focus:text-gray-400 focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
         type="button"
-        data-bs-target="#featuredCommentsSlide"
-        data-bs-slide="prev"
+        data-te-target="#featuredCommentsSlide"
+        data-te-slide="prev"
       >
-        <span
-          className="carousel-control-prev-icon inline-block bg-no-repeat"
-          aria-hidden="true"
-        ></span>
-        <span className="visually-hidden">Previous</span>
+        <span className="inline-block h-8 w-8">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="h-6 w-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15.75 19.5L8.25 12l7.5-7.5"
+            />
+          </svg>
+        </span>
+        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+          Previous
+        </span>
       </button>
       <button
-        className="carousel-control-next absolute top-0 bottom-0 right-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline"
+        className="absolute top-0 bottom-0 right-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-gray-500 opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-gray-600 hover:no-underline hover:opacity-90 hover:outline-none focus:text-gray-400 focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
         type="button"
-        data-bs-target="#featuredCommentsSlide"
-        data-bs-slide="next"
+        data-te-target="#featuredCommentsSlide"
+        data-te-slide="next"
       >
-        <span
-          className="carousel-control-next-icon inline-block bg-no-repeat"
-          aria-hidden="true"
-        ></span>
-        <span className="visually-hidden">Next</span>
+        <span className="inline-block h-8 w-8">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="h-6 w-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M8.25 4.5l7.5 7.5-7.5 7.5"
+            />
+          </svg>
+        </span>
+        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+          Next
+        </span>
       </button>
     </div>
+ 
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ import {
   featuredComments,
 } from "src/utils/fakeData";
 import { FeaturedComments } from "@components/featured-comment";
-import {useBestSallingProducts} from "@hooks/useProduct";
+import { useBestSallingProducts } from "@hooks/useProduct";
 const Home: React.FC = () => {
   const [state] = useState({
     homeSlideData: homeSlideInfo,
@@ -21,86 +21,136 @@ const Home: React.FC = () => {
 
   useEffect(() => {
     // setTheme('light');
-  }, []); 
+  }, []);
 
   const { products } = useBestSallingProducts();
 
   return (
     <Container>
       <div
-        id="homeSlide"
-        className="carousel slide relative"
-        data-bs-ride="carousel"
+        id="carouselSlideImage"
+        className="relative"
+        data-te-carousel-init
+        data-te-carousel-slide
       >
-        <div className="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
-          {homeSlideData?.map((item: any, index: number) => (
-            <button
-              key={index}
-              type="button"
-              data-bs-target="#homeSlide"
-              data-bs-slide-to={index}
-              className={index === 0 ? "active" : ""}
-              aria-current={index === 0 ? "true" : "false"}
-              aria-label="Slide 1"
-            ></button>
-          ))}
+        <div
+          className="absolute right-0 bottom-0 left-0 z-[2] mx-[15%] mb-4 flex list-none justify-center p-0"
+          data-te-carousel-indicators
+        >
+          <button
+            type="button"
+            data-te-target="#carouselSlideImage"
+            data-te-slide-to="0"
+            data-te-carousel-active
+            className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-te-target="#carouselSlideImage"
+            data-te-slide-to="1"
+            className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-te-target="#carouselSlideImage"
+            data-te-slide-to="2"
+            className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
+            aria-label="Slide 3"
+          ></button>
         </div>
-        <div className="carousel-inner relative w-full overflow-hidden ">
-          {homeSlideData?.map((item: any, index: number) => (
-            <div
-              key={index}
-              className={`carousel-item ${
-                index === 0 ? "active" : ""
-              } relative float-left w-full`}
-            >
-              <div className="absolute z-0 top-1/2 left-1/2 -translate-y-2/4 -translate-x-2/4 w-[100%] text-center">
-                {item?.caption && (
-                  <div>
-                    <span className="whitespace-pre-wrap xl:text-3xl text-2xl text-[#fdf6f1] drop-shadow-2xl shadow-2xl shadow-black">
-                      {item?.caption}
-                    </span>
-                  </div>
-                )}
-                {item?.text && (
-                  <button
-                    className="rounded-full h-12 bg-white text-lg min-w-[140px] border-white border hover:bg-transparent hover:text-white"
-                    ref={item?.ref}
-                  >
-                    {item?.text}
-                  </button>
-                )}
+        <div className="relative w-full overflow-hidden after:clear-both after:block after:content-['']">
+          {homeSlideData?.map((item: any, index: number) => {
+            const active = index === 0 ? true : undefined;
+            const hidden = index !== 0;
+            return (
+              <div
+                key={index}
+                className={`relative ${
+                  hidden && "hidden"
+                } float-left -mr-[100%] w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none`}
+                data-te-carousel-item
+                data-te-carousel-active={active}
+              >
+                <div className="absolute z-0 top-1/2 left-1/2 -translate-y-2/4 -translate-x-2/4 w-[100%] text-center">
+                  {item?.caption && (
+                    <div>
+                      <span className="whitespace-pre-wrap xl:text-3xl text-2xl text-[#fdf6f1] drop-shadow-2xl shadow-2xl shadow-black">
+                        {item?.caption}
+                      </span>
+                    </div>
+                  )}
+                  {item?.text && (
+                    <button
+                      className="rounded-full h-12 bg-white text-lg min-w-[140px] border-white border hover:bg-transparent hover:text-white"
+                      ref={item?.ref}
+                    >
+                      {item?.text}
+                    </button>
+                  )}
+                </div>
+                <img
+                  src={item?.url}
+                  className="block w-full object-cover md:h-[40vw] h-[60rem]"
+                  alt="..."
+                />
               </div>
-              <img
-                src={item?.url}
-                className="block w-full object-cover md:h-[40vw] h-[60rem]"
-                alt="..."
-              />
-            </div>
-          ))}
+            );
+          })}
         </div>
         <button
-          className="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
+          className="absolute top-0 bottom-0 left-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
           type="button"
-          data-bs-target="#homeSlide"
-          data-bs-slide="prev"
+          data-te-target="#carouselSlideImage"
+          data-te-slide="prev"
         >
-          <span
-            className="carousel-control-prev-icon inline-block bg-no-repeat"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Previous</span>
+          <span className="inline-block h-8 w-8">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15.75 19.5L8.25 12l7.5-7.5"
+              />
+            </svg>
+          </span>
+          <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+            Previous
+          </span>
         </button>
         <button
-          className="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
+          className="absolute top-0 bottom-0 right-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
           type="button"
-          data-bs-target="#homeSlide"
-          data-bs-slide="next"
+          data-te-target="#carouselSlideImage"
+          data-te-slide="next"
         >
-          <span
-            className="carousel-control-next-icon inline-block bg-no-repeat"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Next</span>
+          <span className="inline-block h-8 w-8">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M8.25 4.5l7.5 7.5-7.5 7.5"
+              />
+            </svg>
+          </span>
+          <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+            Next
+          </span>
         </button>
       </div>
 
