9c8f132e86e93ec690e7283a63028962ffa15590
max <maxpham97@gmail.com>
2023-03-08 11:16:41 +0700

    update profile page

---
+++ src/components/profile/user-profile/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const UserProfile = () => {
+  return (
+    <div>
+      <div className="">Bonjour maxTest</div>
+      <div className="">
+        À partir du tableau de bord de votre compte, vous pouvez visualiser vos commandes récentes,
+        gérer vos adresses de livraison et de facturation ainsi que changer votre mot de passe et
+        les détails de votre compte.
+      </div>
+    </div>
+  );
+};
+
+export default UserProfile;
