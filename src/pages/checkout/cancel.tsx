2e68ad7ddb1ba7cb9cf71329893684fc991bdebd
luckystar360 <ngocnhat95@gmail.com>
2023-03-08 23:36:55 -0800

    update checkout result page

---
+++ src/pages/checkout/cancel.tsx
@@ -0,0 +1,11 @@
+import { Container } from "@components/container";
+
+const Cancel: React.FC = () => {
+    return (
+        <Container>
+            Payment Cancel
+        </Container>
+    );
+};
+
+export default Cancel;
---
+++ src/pages/checkout/success.tsx
@@ -0,0 +1,11 @@
+import { Container } from "@components/container";
+
+const Success: React.FC = () => {
+    return (
+        <Container>
+            Payment Success
+        </Container>
+    );
+};
+
+export default Success;
