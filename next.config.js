789ffd4e8190d3a3c7bcec7d5ed03af5bd27b669
luckystar360 <ngocnhat95@gmail.com>
2023-03-08 20:45:14 -0800

    update pathRewrite

--- next.config.js
+++ next.config.js
@@ -4,14 +4,14 @@ const nextConfig = {
   swcMinify: true,
   rewrites: async () => {
     return [
-      {
-        source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
-      },
-      {
-        source: "/payment/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/payment/:path*`,
-      },
+      // {
+      //   source: "/api/:path*",
+      //   destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
+      // },
+      // {
+      //   source: "/payment/:path*",
+      //   destination: `${process.env.NEXT_PUBLIC_API_URL}/payment/:path*`,
+      // },
     ];
   },
   output: "standalone",
