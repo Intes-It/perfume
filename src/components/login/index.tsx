266911f2d1aaec5e21a031c2ef7ccd7764f5c35c
VietNguyen <nghoangviet2000@hotmail.com>
2023-03-16 20:20:59 +0700

    ..

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,83 +1,86 @@
-import * as React from 'react';
-import ReCAPTCHA from 'react-google-recaptcha';
-import Link from 'next/link';
-import { Routes } from '@definitions/constants';
-import { useForm } from 'react-hook-form';
-import { POST } from '@utils/fetch';
-import { api } from '@utils/apiRoute';
-import useUser from '@hooks/useUser';
-import _ from 'lodash';
-interface loginData{
-  email:string,
-  user_name:string,
-  password:string
-}
+import * as React from "react";
+import ReCAPTCHA from "react-google-recaptcha";
+import Link from "next/link";
+import { Routes } from "@definitions/constants";
+import { useForm } from "react-hook-form";
+import { POST } from "@utils/fetch";
+import { api } from "@utils/apiRoute";
+import useUser from "@hooks/useUser";
+import { FieldValues } from "react-hook-form";
+import _ from "lodash";
+type loginData = {
+  email: string;
+  user_name: string;
+  password: string;
+} & FieldValues;
 type LoginProps = {
-  submit: (value: loginData) => void;
-  checkEmpty?:boolean
+  submit: (value: any) => void;
+  checkEmpty?: boolean;
 };
 
-const Login: React.FC<LoginProps> = ({ submit,checkEmpty }) => {
+const Login: React.FC<LoginProps> = ({ submit, checkEmpty }) => {
   const { register, handleSubmit } = useForm();
   const [state, setState] = React.useState({
-    captchaCode: '',
-   
+    captchaCode: "",
   });
-  const {captchaCode} = state;
-  
+  const { captchaCode } = state;
+
   return (
     <>
-    <form
-      onSubmit={handleSubmit(submit)}
-      className="flex flex-col space-y-5 border rounded-md p-6 text-[14.4px] font-semibold text-[#603813]">
-      <div className="flex flex-col space-y-1">
-        <label>
-          Identifiant ou e-mail <span className="text-red-500 text-[20px] ">*</span>
-        </label>
-        <input
-          {...register('email')}
-          required
-          type="text"
-          id="id"
-          className="px-4 py-3 border border-gray-300 text-black"
-        />
-      </div>
-      <div className="flex flex-col space-y-1">
-        <label>
-          Mot de passe <span className="text-red-500 text-[20px] ">*</span>
-        </label>
-        <input
-          {...register('password')}
-          required
-          type="password"
-          id="password"
-          className="px-4 py-3 border border-gray-300 text-black"
-        />
-      </div>
-      <div className="flex justify-between">
-        <div className="flex items-center space-x-2">
-          <input type="checkbox" id="remember" className="w-4 h-4 " />
-          <label>Se souvenir de moi</label>
+      <form
+        onSubmit={handleSubmit(submit)}
+        className="flex flex-col space-y-5 border rounded-md p-6 text-[14.4px] font-semibold text-[#603813]"
+      >
+        <div className="flex flex-col space-y-1">
+          <label>
+            Identifiant ou e-mail{" "}
+            <span className="text-red-500 text-[20px] ">*</span>
+          </label>
+          <input
+            {...register("email")}
+            required
+            type="text"
+            id="id"
+            className="px-4 py-3 border border-gray-300 text-black"
+          />
         </div>
-      </div>
-      <ReCAPTCHA
-        sitekey="6LcF1twkAAAAAMKsDQ71Bhktr3g0Q66sZM5bngyA"
-        onChange={(captchaCode)=>{
-          setState((pre)=>({...pre, captchaCode: captchaCode || ''})) 
-        }}
-      />
-      <div>
-        <button
-          type="submit"
-          disabled={_.isEmpty(captchaCode)}
-          className="w-[200px] px-4 py-3 text-[16px] uppercase font-semibold text-white  bg-[#603813] rounded-md shadow hover:bg-black disabled:bg-gray-300">
-          Identification
-        </button>
-      </div>
-      <Link href={Routes.myAccount.children.forgotPassword.route}>
-        <a className="text-base  text-brow-300">Mot de passe perdu ?</a>
-      </Link>
-    </form>
+        <div className="flex flex-col space-y-1">
+          <label>
+            Mot de passe <span className="text-red-500 text-[20px] ">*</span>
+          </label>
+          <input
+            {...register("password")}
+            required
+            type="password"
+            id="password"
+            className="px-4 py-3 border border-gray-300 text-black"
+          />
+        </div>
+        <div className="flex justify-between">
+          <div className="flex items-center space-x-2">
+            <input type="checkbox" id="remember" className="w-4 h-4 " />
+            <label>Se souvenir de moi</label>
+          </div>
+        </div>
+        <ReCAPTCHA
+          sitekey="6LcF1twkAAAAAMKsDQ71Bhktr3g0Q66sZM5bngyA"
+          onChange={(captchaCode) => {
+            setState((pre) => ({ ...pre, captchaCode: captchaCode || "" }));
+          }}
+        />
+        <div>
+          <button
+            type="submit"
+            disabled={_.isEmpty(captchaCode)}
+            className="w-[200px] px-4 py-3 text-[16px] uppercase font-semibold text-white  bg-[#603813] rounded-md shadow hover:bg-black disabled:bg-gray-300"
+          >
+            Identification
+          </button>
+        </div>
+        <Link href={Routes.myAccount.children.forgotPassword.route}>
+          <a className="text-base  text-brow-300">Mot de passe perdu ?</a>
+        </Link>
+      </form>
     </>
   );
 };
