713e28252b1f4f74dbaafc11c42cd5affc2b7719
luckystar360 <ngocnhat95@gmail.com>
2023-05-26 10:37:52 +0700

    fix product price

--- src/pages/product/[product-id]/index.tsx
+++ src/pages/product/[product-id]/index.tsx
@@ -296,11 +296,14 @@ const ProductDetail: React.FC<
             <Rating score={product?.evaluate || 0} />
             <span>{`( 0 avis client)`}</span>
           </div>
-          {/*<div className="my-2">*/}
-          {/*  <span className="text-[#383e42] text-[24px] font-semibold">*/}
-          {/*    {formatCurrency(String(product?.price))} €*/}
-          {/*  </span>*/}
-          {/*</div>*/}
+          {
+            _.isEmpty(product?.capacity) &&
+            <div className="my-2">
+              <span className="text-[#383e42] text-[24px] font-semibold">
+                {formatCurrency(String(product?.price))} €
+              </span>
+            </div>
+          }
           {/* color */}
           <div className="my-3">
             {_.isEmpty(product?.color) ? null : (
