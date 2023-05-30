4b11b63858b101199fb16a1b567788086a12fadf
Việt Nguyễn <nghoangviet2000@hotmail.com>
2023-05-30 07:44:01 +0000

    Vietnh

--- src/components/blog/index.tsx
+++ src/components/blog/index.tsx
@@ -1,5 +1,6 @@
-import { Container } from '@components/container';
-import React from 'react';
+import { Container } from "@components/container";
+import Link from "next/link";
+import React from "react";
 
 type BlogProps = {
   image?: string;
@@ -8,36 +9,35 @@ type BlogProps = {
   date?: string;
 };
 
-const BlogComponent: React.FC<BlogProps> = ({ image, title, subTitle, date }) => {
-  console.log(image);
-  console.log(title);
-  console.log("here");
+const BlogComponent: React.FC<BlogProps> = ({
+  image,
+  title,
+  subTitle,
+  date,
+}) => {
   return (
     <Container>
-      <div className="flex flex-col tablet:flex-row lg:flex-row tablet:flex-wrap justify-between mx-3 mt-20 text-[12px] text-[#6a5950]">
-        <div className="tablet:w-[45vw] lg:w-[30vw] shadow-lg rounded-sm mb-5 relative hover:shadow-2xl ">
-          <a href="/blog/blog1">
+      <Link href="/blog/blog1">
+        <div className="flex flex-col tablet:flex-row lg:flex-row tablet:flex-wrap justify-between mx-3 mt-20 text-[12px] text-[#6a5950]">
+          <div className="tablet:w-[45vw] lg:w-[30vw] shadow-lg rounded-sm mb-5 relative hover:shadow-2xl cursor-pointer">
             <img src={image} alt="" />
-          </a>
-          <div className="absolute top-3 right-3 h-[26px] w-[90px]  rounded-[50px]  bg-[#818A91] text-center leading-[26px] text-[#ffffff]">
-            JOURNAL
-          </div>
-          <div className="flex flex-col justify-between h-[80px] mx-9 my-5">
-            <h3 style={{ fontSize: '21px' }}>
-              <a href="/blog/blog1">{title}</a>
-            </h3>
-            <a className="font-semibold" href="/blog/blog1">
-              {subTitle}
-            </a>
-          </div>
-          <div className="flex items-center h-[47px] border-t  text-[#adadad]">
-            <span style={{ margin: '0px 12px 0px 36px' }}>{date}</span>
-            <li>Aucun commentaire</li>
+
+            <div className="absolute top-3 right-3 h-[26px] w-[90px]  rounded-[50px]  bg-[#818A91] text-center leading-[26px] text-[#ffffff]">
+              JOURNAL
+            </div>
+            <div className="flex flex-col justify-between h-[80px] mx-9 my-5 ">
+              <p className="lg:text-[21px]">
+                <a>{title}</a>
+              </p>
+              <p className="font-semibold">{subTitle}</p>
+            </div>
+            <div className="flex items-center h-[45px] border-t  text-[#adadad]">
+              <span style={{ margin: "0px 12px 0px 36px" }}>{date}</span>
+              <li>Aucun commentaire</li>
+            </div>
           </div>
         </div>
-        {/* <div className=""></div>
-      <div className=""></div> */}
-      </div>
+      </Link>
     </Container>
   );
 };
