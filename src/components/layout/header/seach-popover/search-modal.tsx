0b400ddd934ba4a333a7205cc94901c303ce1eb6
Việt Nguyễn <nghoangviet2000@hotmail.com>
2023-05-30 07:13:41 +0000

    Vietnh

--- src/components/layout/header/seach-popover/search-modal.tsx
+++ src/components/layout/header/seach-popover/search-modal.tsx
@@ -1,23 +1,23 @@
-import { useRouter } from 'next/router';
-import React, { useState } from 'react';
+import { useRouter } from "next/router";
+import React, { useState } from "react";
 type SearchProps = {
   isVisible: boolean;
   onClose: () => void;
 };
 const SearchModal: React.FC<SearchProps> = ({ isVisible, onClose }) => {
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState("");
   const router = useRouter();
 
   if (!isVisible) return null;
   const handleClose = (e: any) => {
-    if (e.target.id === 'wrapper') onClose();
+    if (e.target.id === "wrapper") onClose();
   };
 
   const handleKeyDown = (event: any) => {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
       onClose();
       router.push({
-        pathname: '/search',
+        pathname: "/search",
         query: {
           result: value,
         },
@@ -28,17 +28,19 @@ const SearchModal: React.FC<SearchProps> = ({ isVisible, onClose }) => {
     <div
       id="wrapper"
       onClick={handleClose}
-      className="animate-crescendo fixed z-50 inset-0 bg-black bg-opacity-75 flex justify-center items-center m-0">
+      className="animate-crescendo fixed z-50 inset-0 bg-black bg-opacity-75 flex justify-center items-center m-0"
+    >
       <button
         onClick={() => onClose()}
-        className="fixed top-[50%] right-[50px] text-white text-xl place-self-end  mobile:right-[15px]">
+        className="fixed top-[50%] right-[50px] text-white text-xl place-self-end  mobile:right-[15px]"
+      >
         X
       </button>
       <input
         onChange={(e) => setValue(e.target.value)}
         onKeyDown={handleKeyDown}
         type="search"
-        className="form input px-4 py-4 w-[60vw] bg-black bg-opacity-0 focus:outline-none text-white text-[50px] text-center border-b-2 mobile:text-[30px]"
+        className="form input px-4 py-4 w-[60vw] bg-black bg-opacity-0 focus:outline-none text-white lg:text-[50px] text-center border-b-2 sm:text-[30px]"
         placeholder="Recherche..."
       />
     </div>
