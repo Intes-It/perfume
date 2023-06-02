b159244476b72a4451583806b87609e2a9a0b7ee
Nguyễn Việt <nghoangviet2000@hotmail.com>
2023-06-02 09:10:57 +0700

    add be url

--- src/pages/api/[...all].ts
+++ src/pages/api/[...all].ts
@@ -12,7 +12,7 @@ export const config = {
 export default (req: NextApiRequest, res: NextApiResponse) => {
     return httpProxyMiddleware(req, res, {
         // You can use the `http-proxy` option
-        target: process.env.NEXT_PUBLIC_API_URL,
+        target: 'http://34.163.185.96',
         // In addition, you can use the `pathRewrite` option provided by `next-http-proxy-middleware`
         pathRewrite: [ 
             {
