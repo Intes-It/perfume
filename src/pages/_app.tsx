7d4eaa0986623d73eea5ff13e0a8e4ee287017aa
Việt Nguyễn <nghoangviet2000@hotmail.com>
2023-06-14 10:09:29 +0000

    Vie

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,15 +10,13 @@ import "tailwindcss/tailwind.css";
 import { Hydrate } from "react-query/hydration";
 import { Provider } from "react-redux";
 import Head from "next/head";
-import '@styles/styles.css'
+import "@styles/styles.css";
 import LoadingIndicator from "@components/loading-indicator";
 import { Layout } from "@components";
 import GlobalStyle from "@styles/globalStyles";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
-import Script from 'next/script'
-import { PersistGate } from "redux-persist/integration/react";
-import { persistStore } from "redux-persist"; 
+import { persistStore } from "redux-persist";
 
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
@@ -28,14 +26,15 @@ function MyApp({
 }: AppProps<{ dehydratedState: DehydratedState }>): JSX.Element {
   const [state, setState] = useState({ queries: 0, mutations: 0 });
   const { queries, mutations } = state;
-  
-  let queryCount = 0, mutationCount = 0;
+
+  let queryCount = 0,
+    mutationCount = 0;
 
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
         refetchOnWindowFocus: false,
-        staleTime: 3000, // 5 minutes   
+        staleTime: 3000, // 5 minutes
       },
       mutations: {
         onMutate: (variables: any) => {
@@ -43,11 +42,11 @@ function MyApp({
           setState((pre) => ({ ...pre, mutations: mutationCount }));
         },
         onError: (error, variables, rollback: any) => {
-          console.log('error')
+          console.log("error");
           // Nếu bạn muốn xử lý các lỗi xảy ra trong quá trình mutation
         },
         onSuccess: () => {
-          console.log('onSuccess')
+          console.log("onSuccess");
           // Nếu bạn muốn xử lý các thành công xảy ra trong quá trình mutation
         },
         onSettled: (data, error, onSettled) => {
@@ -57,26 +56,25 @@ function MyApp({
       },
     },
   });
- 
-  queryClient.getQueryCache().subscribe((event: any) => { 
-    if (event?.['action']?.type === 'fetch') {
+
+  queryClient.getQueryCache().subscribe((event: any) => {
+    if (event?.["action"]?.type === "fetch") {
       queryCount = queryCount + 1;
       setState((pre) => ({ ...pre, queries: queryCount }));
-    }
-    else if (event?.['action']?.type === 'success' || event?.['action']?.type === 'error') {
+    } else if (
+      event?.["action"]?.type === "success" ||
+      event?.["action"]?.type === "error"
+    ) {
       queryCount = queryCount - 1;
       setState((pre) => ({ ...pre, queries: queryCount }));
     }
-  })
-  persistStore(store)
+  });
+  persistStore(store);
   return (
     <>
-      <Head>
-        <meta charSet="utf8" />
-        <link rel="icon" href="/images/icon.png" />
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
-      <Script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js" />
+      {/* <Head>
+       
+      </Head> */}
       <div>
         <QueryClientProvider client={queryClient}>
           <GlobalStyle />
