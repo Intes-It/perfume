805bf69e112bf8f4d4508ec0a8b012a6da044ff4
Nguyễn Việt <nghoangviet2000@hotmail.com>
2023-10-29 20:11:40 +0700

    add code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,7 +22,7 @@ const Home: React.FC = () => {
 
   const homeSlideInfo = [
     {
-      url: "https://firebasestorage.googleapis.com/v0/b/securip-dev.appspot.com/o/naturefeerique%2Fslide1-1-scaled-1.webp?alt=media&token=2d469600-5784-4598-b167-49424e2ca81a",
+      url: "https://firebasestorage.googleapis.com/v0/b/secu-rip-ad662.appspot.com/o/naturefeerique%2Fslide1-1-scaled-1.webp?alt=media&token=20f8ac0b-f5b5-47d3-a82d-50ac43b1ef1b_gl=1*ccc7c0*_ga*MjA1MzU1MTI1NS4xNjgzODExNTg1*_ga_CW55HF8NVT*MTY5ODU4MzkxNy40MS4xLjE2OTg1ODQ3MDcuNjAuMC4w",
       caption: "",
       text: (
         <div
@@ -39,10 +39,10 @@ const Home: React.FC = () => {
           </button>
         </div>
       ),
-      mobile_img: 'https://firebasestorage.googleapis.com/v0/b/securip-dev.appspot.com/o/naturefeerique%2Fslide3-mobile.webp?alt=media&token=ed61195f-c3d0-4d33-8d6a-81ab87893af5'
+      mobile_img: 'https://firebasestorage.googleapis.com/v0/b/secu-rip-ad662.appspot.com/o/naturefeerique%2F8.webp?alt=media&token=d2f22501-0aab-4da1-96bd-bd6cf0261125&_gl=1*np5r2p*_ga*MjA1MzU1MTI1NS4xNjgzODExNTg1*_ga_CW55HF8NVT*MTY5ODU4MzkxNy40MS4xLjE2OTg1ODQ3OTYuNDMuMC4w'
     },
     {
-      url: "https://firebasestorage.googleapis.com/v0/b/securip-dev.appspot.com/o/naturefeerique%2Fs3-1.webp?alt=media&token=3c8530d4-b62b-4cfa-837f-70631fecc90d",
+      url: "https://firebasestorage.googleapis.com/v0/b/secu-rip-ad662.appspot.com/o/naturefeerique%2Fs3-1.webp?alt=media&token=1ece2091-85e6-4550-bb73-f91e93deb792_gl=1*9kzw9i*_ga*MjA1MzU1MTI1NS4xNjgzODExNTg1*_ga_CW55HF8NVT*MTY5ODU4MzkxNy40MS4xLjE2OTg1ODQ2NTIuMy4wLjA.",
       text: (
         <div
           className="absolute z-0   -translate-y-2/4 -translate-x-2/4 w-[100%] text-center"
@@ -60,10 +60,10 @@ const Home: React.FC = () => {
           </button>
         </div>
       ),
-      mobile_img: 'https://firebasestorage.googleapis.com/v0/b/securip-dev.appspot.com/o/naturefeerique%2Fslide2-mobile.webp?alt=media&token=8f15472c-e871-4b1d-b563-4c6fd3ecf51c'
+      mobile_img: 'https://firebasestorage.googleapis.com/v0/b/secu-rip-ad662.appspot.com/o/naturefeerique%2F8.webp?alt=media&token=d2f22501-0aab-4da1-96bd-bd6cf0261125_gl=1*10q6dwr*_ga*MjA1MzU1MTI1NS4xNjgzODExNTg1*_ga_CW55HF8NVT*MTY5ODU4MzkxNy40MS4xLjE2OTg1ODUwMjMuNjAuMC4w'
     },
     {
-      url: "https://firebasestorage.googleapis.com/v0/b/securip-dev.appspot.com/o/naturefeerique%2Fmainof.webp?alt=media&token=6d48328f-d1b6-4efd-9b86-a0fd144cbdac",
+      url: "https://firebasestorage.googleapis.com/v0/b/secu-rip-ad662.appspot.com/o/naturefeerique%2Fmainof.webp?alt=media&token=251400bb-308e-468e-a68a-d7ebb013d050&_gl=1*w4d886*_ga*MjA1MzU1MTI1NS4xNjgzODExNTg1*_ga_CW55HF8NVT*MTY5ODU4MzkxNy40MS4xLjE2OTg1ODQxMTEuMTMuMC4w",
       text: (
         <div
           className="absolute z-0  -translate-y-2/4 -translate-x-2/4 w-[100%] text-center"
@@ -79,7 +79,7 @@ const Home: React.FC = () => {
           </button>
         </div>
       ),
-      mobile_img:'https://firebasestorage.googleapis.com/v0/b/securip-dev.appspot.com/o/naturefeerique%2Fslide1-mobile.webp?alt=media&token=b92e8a37-93c4-450a-9d40-ba20252c0090'
+      mobile_img:'https://firebasestorage.googleapis.com/v0/b/secu-rip-ad662.appspot.com/o/naturefeerique%2F10.webp?alt=media&token=64dd0cbd-fce9-4796-bec5-8591fa9d7ce8_gl=1*q1notb*_ga*MjA1MzU1MTI1NS4xNjgzODExNTg1*_ga_CW55HF8NVT*MTY5ODU4MzkxNy40MS4xLjE2OTg1ODQ4ODIuNjAuMC4w'
 
     },
   ];
