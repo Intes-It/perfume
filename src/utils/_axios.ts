ba657bc1d3264a65c897eb3f912d4f3668adaa72
Aloysius Nguyen <nghoangviet2000@hotmail.com>
2024-04-20 14:55:29 +0700

    Update _axios.ts

--- src/utils/_axios.ts
+++ src/utils/_axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  baseURL:'https://parfumnhapkhau.com',
 });
 
 instance.defaults.xsrfHeaderName = "X-CSRFToken";
