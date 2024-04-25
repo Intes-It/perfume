caece4f3fd822373fca187c7ab2307030b4cddd9
hoang <nqhoang23@gmail.com>
2024-04-25 16:29:59 +0700

    fix: fix loading

--- src/hooks/useUser/index.tsx
+++ src/hooks/useUser/index.tsx
@@ -1,11 +1,13 @@
-import { api } from '@utils/apiRoute';
-import { GET, POST } from '@utils/fetch';
-import { useMutation, useQuery, useQueryClient } from 'react-query';
- 
+import { api } from "@utils/apiRoute";
+import { GET, POST } from "@utils/fetch";
+import { getCookie } from "cookies-next";
+import { useMutation, useQuery, useQueryClient } from "react-query";
+
 const useUser = () => {
   const queryClient = useQueryClient();
-  const user = useQuery("get-profile", getProfile); 
+  const csrfToken = getCookie("csrftoken");
 
+  const user = csrfToken ? useQuery("get-profile", getProfile) : { data: null };
   //fetch data
   async function getProfile() {
     const res = await GET(api.getProfile);
@@ -13,30 +15,26 @@ const useUser = () => {
     return res.data;
   }
 
-  async function login(data: any) { 
+  async function login(data: any) {
     return await POST(api.login, data);
   }
 
-  async function register (data: any){
+  async function register(data: any) {
     return await POST(api.register, data);
-  } 
-
-  const loginAccount = useMutation('login', login);
-  const registerAccount = useMutation('register', register,
-  {
-    onSuccess:()=>{ 
-      queryClient.invalidateQueries('get-profile');
-
-    }
   }
-  )
 
+  const loginAccount = useMutation("login", login);
+  const registerAccount = useMutation("register", register, {
+    onSuccess: () => {
+      queryClient.invalidateQueries("get-profile");
+    },
+  });
 
   return {
     user: user.data,
     isAuthenticated: Boolean(user?.data?.id),
     loginAccount: loginAccount.mutateAsync,
-    registerAccount: registerAccount.mutateAsync
+    registerAccount: registerAccount.mutateAsync,
   };
 };
 
