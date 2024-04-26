daf8c1477a5751a8a89e28d554f61578ca2660c4
Cyril <secur365.suite@gmail.com>
2024-04-26 09:44:58 +0700

    🔒️

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -67,7 +67,7 @@ const Checkout: React.FC = () => {
   // const stripePromise = loadStripe(
   //  "pk_live_51Mc4mkLl7R805p8JLhoVvkkN3QPMnPGIRWOEfZVuW6ZEQoL9bUmEiwcusKcVCXPNyzwWdayXXWA8Dc7KwCqiCqX600dz3hbcEv"
   //  );
-  const stripePromise = loadStripe(process.env.STRIPE_KEY as string);
+  const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY as string);
   const hasError = () => {
     switch (activeTab) {
       case 0:
